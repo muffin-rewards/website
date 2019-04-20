a870651b928a4ec640ef439f2eeee28c4343a906
Pavel Koch <kouks.koch@gmail.com>
2019-04-20 16:14:56 +0100

    added offer info and faq takeovers

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,30 @@
 <template>
   <div>
     <router-view/>
+
+    <footer class="footer has-text-centered">
+      <a class="title is-underlined is-3 has-text-white" @click="faqsActive = true">FAQs</a>
+    </footer>
+
+    <Faqs :active="faqsActive" @close="faqsActive = false"></Faqs>
   </div>
 </template>
 
 <script lang="ts">
+import Faqs from '@/components/takeovers/Faqs.vue'
 import { Component, Vue } from 'vue-property-decorator'
 
-@Component
+@Component({
+  components: { Faqs },
+})
 export default class extends Vue {
-  //
+
+  /**
+   * Whether FAQs takeover is active.
+   * TODO: Extract to store?
+   */
+  public faqsActive: boolean = false
+
 }
 </script>
 
---
+++ src/components/takeovers/Faqs.vue
@@ -0,0 +1,43 @@
+<template>
+  <TakeOver :active="active" @close="$emit('close')">
+    <template #title>
+      <p class="title is-3 has-text-white">FAQs</p>
+    </template>
+
+    <template #body>
+      <div class="content">
+        <p>
+          <b>What is Muffin?</b><br>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugiat debitis, unde autem consectetur dolore.
+        </p>
+
+        <p>
+          <b>What is Muffin?</b><br>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugiat debitis, unde autem consectetur dolore.
+        </p>
+      </div>
+    </template>
+
+    <template #foot>
+      <Action secondary block large @click="$emit('close')">Close</Action>
+    </template>
+  </TakeOver>
+</template>
+
+<script lang="ts">
+import TakeOver from '@/components/TakeOver.vue'
+import { Component, Vue, Prop } from 'vue-property-decorator'
+
+@Component({
+  components: { TakeOver },
+})
+export default class extends Vue {
+
+  /**
+   * Whether the take over is active.
+   */
+  @Prop(Boolean)
+  public active: boolean
+
+}
+</script>
